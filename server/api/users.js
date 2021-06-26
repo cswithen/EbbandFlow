7c322df78e4b7e030f95a489bb146e2795bc625e
Cody Swithenbank <cody.swithenbank@gmail.com>
2021-06-26 02:54:05 -0700

    added in api routes for single user GET and POST

--- server/api/users.js
+++ server/api/users.js
@@ -1,15 +1,48 @@
 const router = require("express").Router();
 const {models: {User}} = require('../db')
+const {adminCheck, userCheck} = require ('./authentication/authentication')
 
-router.get("/", async (req, res, next) => {
+//GET all users  '/api/users/'
+router.get('/', adminCheck, async (req, res, next) => {
   try {
     const users = await User.findAll({
-      attributes:['id','username']
+      attributes: ['id', 'username'],
     })
-    res.json(users)
+    if(!users) {
+      return res.status(404).send("Users not found in database")
+    }
+    return res.json(users)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//GET one user '/api/users/:userId
+router.get("/:userId", userCheck, async (req, res, next) => {
+  try {
+    const {userId} = req.params
+    const user = await User.findByPk( userId, {
+      include: []
+    })
+    if(!user) {
+      return res.status(404).send("User not found in database")
+    }
+    return res.json(user)
   } catch (error) {
     next(error)
   }
 });
+
+// POST create new user '/api/users'
+router.post('/', async (req, res, next) => {
+  try {
+    const data = req.body
+    const {dataValues} = await User.create(data)
+
+    return res.status(201).json(dataValues)
+  } catch (error) {
+    next (error)
+  }
+})
 
 module.exports = router;
