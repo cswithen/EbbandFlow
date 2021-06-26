fe68f56819f62413636aa02c4f7a72d01875ff41
Cody Swithenbank <cody.swithenbank@gmail.com>
2021-06-26 15:30:40 -0700

    added seed data and connections for workouts and poses

--- script/seed.js
+++ script/seed.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const {db } = require('../server/db')
-const {User, Pose} = require('../server/db/models')
+const {User, Pose, Workout} = require('../server/db/models')
 const posesData = require('./poses.json')
 
 async function seed() {
@@ -20,8 +20,21 @@ async function seed() {
     User.create({ username: 'murphy', password: '123'})
   ])
 
+  const workouts = await Promise.all([
+    Workout.create({name: 'Slow Flow'}),
+    Workout.create({name: 'Power Flow'})
+  ])
+
+ await workouts[0].setUser(users[0])
+ await workouts[1].setUser(users[0])
+
+ await workouts[0].addPoses(poses)
+ await workouts[1].addPoses(poses[0])
+ await workouts[1].addPoses(poses[3])
+
   console.log(`seeded ${users.length} users`)
   console.log(`seeded ${poses.length} poses`)
+  console.log(`seeded ${workouts.length} workouts`)
   console.log(`seeded successfully`)
   return {
     users: {
