e3b633bd179bf1e87835a7abdaa966372170f75d
Cody Swithenbank <cody.swithenbank@gmail.com>
2021-06-25 19:00:25 -0700

    set up db sync with session and passports

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,10 @@
 const Sequelize = require('sequelize');
+const pkg = require('../../package.json')
 
-const db = new Sequelize('postgres://localhost:5432/boilerMaker', {
-  logging: false
+const databaseName = pkg.name
+
+const db = new Sequelize(`postgres://localhost:5432/${databaseName}`, {
+  logging: false,
 })
 
 module.exports = db
--- server/index.js
+++ server/index.js
@@ -1,22 +1,95 @@
-const PORT = process.env.PORT || 3000;
-const app = require("./app");
-const {db} = require("./db/index");
-const seed = require('../script/seed')
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const { db } = require("./db/index");
+const sessionStore = new SequelizeStore({ db });
+const PORT = 3000;
 
+const app = express();
+module.exports = app;
 
+// passport registration
+passport.serializeUser((user, done) => done(null, user.id));
 
-const init = async () => {
+passport.deserializeUser(async (id, done) => {
   try {
-    // if(process.env.SEED === 'true') {
-      await seed()
-    // }
-    // else {
-    //   await db.sync()
-    // }
-    app.listen(PORT, () => console.log(`Listening on ${PORT}`))
-  } catch (ex) {
-    console.log(ex)
+    const user = await db.models.user.findbyPk(id);
+    done(null, user);
+  } catch (error) {
+    done(error);
   }
+});
+
+const generateApp = () => {
+  app.enable("trust proxy");
+
+  //logging middleware
+  const morgan = require("morgan");
+  app.use(morgan("dev"));
+
+  //bodyParsing middleware
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use(
+    session({
+      secret: "whatASecret",
+      store: sessionStore,
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  //api routes
+  app.use("/api", require("./api"));
+  app.use("/auth", require("./auth"));
+
+  //static assets
+  app.use(express.static(path.join(__dirname, "../public")));
+
+  app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      const err = new Error("Not found");
+      err.status = 404;
+      next(err);
+    } else {
+      next();
+    }
+  });
+
+  app.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "..", "public/index.html"))
+  );
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "..", "public/index.html"));
+  });
+
+  app.use(function (error, req, res, next) {
+    console.error(error);
+    console.error(error.stack);
+    res
+      .status(error.status || 500)
+      .send(error.message || "Internal server error.");
+  });
+};
+
+const appListen = () => {
+  app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+};
+
+// const dbSync = async () => await db.sync()
+
+async function init() {
+  await sessionStore.sync();
+  await db.sync();
+  await generateApp();
+  await appListen();
 }
 
-init()
+init();
