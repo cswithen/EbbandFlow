805768e288aa4a24696099c910aeadc4eb0d6a0c
Cody Swithenbank <cody.swithenbank@gmail.com>
2021-06-27 14:28:59 -0700

    styled edit workout page

--- client/components/EditWorkout.js
+++ client/components/EditWorkout.js
@@ -6,10 +6,35 @@ import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { PosesContext } from "../contexts/posesContext";
 import styled from "styled-components";
 
+const FormDiv = styled.div`
+  width: 50%;
+  margin: auto;
+`;
+
+const AdditionalPoses = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 50em;
+`;
+
+const BigContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+const AddContainer = styled.div`
+  flex-wrap: wrap;
+  flex-shrink: 1;
+`;
+
 const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
   border-radius: 2px;
+  flex: 2 0 auto;
+  display: flex;
+  flex-direction: column;
+  max-width: 25em;
 `;
 const Title = styled.h3`
   padding: 8px;
@@ -30,15 +55,31 @@ const PoseContainer = styled.div`
 `;
 
 const Button = styled.button`
-background: ${props => props.primary ? '#639cbf' : '#d99379' };
-color: ${props => props.primary ? '#f2ebec' : '#1e403c' };
+  cursor: pointer;
+  background: ${(props) => (props.primary ? "#639cbf" : "#d99379")};
+  color: ${(props) => (props.primary ? "#f2ebec" : "#f2ebec")};
 
-font-size: 1em;
-margin: 0.25em;
-padding: 0.25em 0.25em;
-border: 0px solid ${props => props.primary ? '#f2ebec' : '' };
-border-radius: 3px;
-`
+  font-size: 1em;
+  margin: 0.25em;
+  padding: 0.25em 0.25em;
+  border: 0px solid ${(props) => (props.primary ? "#FFFFFF" : "")};
+  border-radius: 3px;
+  transition: 0.5s all ease-out;
+
+  &:hover {
+    color: #735449;
+    background-color: ${(props) => (props.primary ? "#ffffff" : "#ffffff")}
+`;
+
+const Input = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  background-color: #d99379;
+  color: #ffffff;
+  border: none;
+  border-radius: 3px;
+  font-family: Montserrat;
+`;
 
 const EditWorkout = ({ match }) => {
   const workoutId = +match.params.workoutId;
@@ -157,70 +198,80 @@ const EditWorkout = ({ match }) => {
   return (
     <div>
       <section>
-        <form>
-          <div>
-            <label htmlFor="sequenceName">Name</label>
-            <input
-              type="text"
-              value={name}
-              onChange={(event) => setName(event.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="spotifyUrl">Spotify Url</label>
-            <input
-              type="text"
-              value={spotify}
-              onChange={(event) => setSpotify(event.target.value)}
-            />
-          </div>
-        </form>
-        <Container>
-          <Title>{workout.name}</Title>
-          <DragDropContext onDragEnd={onDragEnd}>
-            <Droppable droppableId="1">
-              {(provided, snapshot) => (
-                <TaskList
-                  ref={provided.innerRef}
-                  {...provided.droppableProps}
-                  isDraggingOver={snapshot.isDraggingOver}
-                >
-                  {workoutList.map((pose, index) => (
-                    <Draggable
-                      key={pose.id}
-                      draggableId={pose.name}
-                      index={index}
-                    >
-                      {(provided, snapshot) => (
-                        <PoseContainer
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          ref={provided.innerRef}
-                          isDragging={snapshot.isDragging}
-                          key={pose.id}
-                        >
-                          {index + 1}
-                          {". "}
-                          {pose.name}
-                        </PoseContainer>
-                      )}
-                    </Draggable>
-                  ))}
-                  {provided.placeholder}
-                </TaskList>
-              )}
-            </Droppable>
-          </DragDropContext>
-        </Container>
-        <Button type="submit" onClick={handleSubmit} primary>
-          Submit
-        </Button>
-        <h2>Add Some More </h2>
-        {poses.map((pose) => (
-          <Button key={pose.id} name={pose.name} onClick={handleClick}>
-            {pose.name}
-          </Button>
-        ))}
+        <FormDiv>
+          <form>
+            <div>
+              <label htmlFor="sequenceName">Name</label>
+              <Input
+                type="text"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="spotifyUrl">Spotify Url</label>
+              <Input
+                type="text"
+                value={spotify}
+                onChange={(event) => setSpotify(event.target.value)}
+              />
+            </div>
+          </form>
+        </FormDiv>
+        <BigContainer>
+          <Container>
+            <Title>{workout.name}</Title>
+            <DragDropContext onDragEnd={onDragEnd}>
+              <Droppable droppableId="1">
+                {(provided, snapshot) => (
+                  <TaskList
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                    isDraggingOver={snapshot.isDraggingOver}
+                  >
+                    {workoutList.map((pose, index) => (
+                      <Draggable
+                        key={pose.id}
+                        draggableId={pose.name}
+                        index={index}
+                      >
+                        {(provided, snapshot) => (
+                          <PoseContainer
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            ref={provided.innerRef}
+                            isDragging={snapshot.isDragging}
+                            key={pose.id}
+                          >
+                            {index + 1}
+                            {". "}
+                            {pose.name}
+                          </PoseContainer>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </TaskList>
+                )}
+              </Droppable>
+            </DragDropContext>
+            <Button type="submit" onClick={handleSubmit} primary>
+              Submit
+            </Button>
+          </Container>
+          <AddContainer>
+            <h2>Add Some More </h2>
+            <AdditionalPoses>
+              <div>
+                {poses.map((pose) => (
+                  <Button key={pose.id} name={pose.name} onClick={handleClick}>
+                    {pose.name}
+                  </Button>
+                ))}
+              </div>
+            </AdditionalPoses>
+          </AddContainer>
+        </BigContainer>
       </section>
     </div>
   );
